7047b3e02de9a6575bae03b4aa0196b01816a365
Gustavo Morales <gus.morales@me.com>
2022-05-11 17:15:57 -0400

    add ssr and no ssr page

---
+++ src/services/pokeapi.ts
@@ -0,0 +1,10 @@
+export default async function getPokemons() {
+  try {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?offset=20&limit=200`
+    );
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {}
+}
